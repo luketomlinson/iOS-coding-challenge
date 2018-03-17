157bc7a6710d795c146b8502662d59d47e84626b
James R T Bossert <james.bossert@willowtreeapps.com>
2018-03-17 16:30:59 -0400

    jrtb's histogram

---
+++ histogram/jrtb.swift
@@ -0,0 +1,33 @@
+//: jrtb's histogram
+
+import UIKit
+
+func printHistogram(with values: [Int]) {
+    print("Input: \(values)")
+    print("Output:")
+
+    var uniques = Set<Int>()
+    values.map { uniques.insert($0) }
+
+    let count = uniques.map { value in values.filter { $0 == value }.count }
+    
+    let maxX = count.count
+    let maxY = count.max() ?? 0
+
+    for y in (0 ..< maxY).reversed() {
+        for x in 0 ..< maxX {
+            if count[x] > y {
+                print("*", terminator: "")
+            } else {
+                print(" ", terminator: "")
+            }
+        }
+        print()
+    }
+    
+    uniques.map { print("\($0)", terminator: "") }
+    print("\n")
+}
+
+printHistogram(with: [3,4,2,1,6,6,4,4])
+printHistogram(with: [8,8,8,8,6,1,1,8,1,4])
