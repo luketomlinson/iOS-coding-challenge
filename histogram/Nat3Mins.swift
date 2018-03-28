72c28732969b755cc3308741804a0290b9b54bc6
Nat Hillard <nat.hillard@willowtreeapps.com>
2018-03-28 18:48:55 -0400

    Updating solution to work

--- histogram/Nat3Mins.swift
+++ histogram/Nat3Mins.swift
@@ -1,19 +1,20 @@
 //: Playground - noun: a place where people can play
 
-//NATS THREE MINUTE SOLUTION
 import Foundation
 
 func printHistogram(values:[Int]) {
     let set = NSCountedSet(array:values)
     let counts = set.map { set.count(for:$0) }
-    let height = counts.max()
-    // SOMETHING LIKE THS
-//    for (i, num) in set.enumerated() {
-//        for height in height...1 {
-//            print(i, num, height)
-//        }
-//    }
-    
+    let height = counts.max()!
+    for line in stride(from: height, to: 0, by: -1) {
+        for count in counts {
+            let char = (count >= line) ? "*" : " "
+            print(" ", char, terminator: "")
+        }
+        print("")
+    }
+    print("")
+    print(Array(set))
 }
 
 printHistogram(values: [3,4,2,1,6,6,4,4])
